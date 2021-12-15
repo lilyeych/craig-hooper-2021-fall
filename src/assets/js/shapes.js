e3fe0a8041ed0e6bac0b05617481db06bcedb2e7
Lily Han <lily.han@fcbsixconsulting.com>
2021-12-14 21:38:29 -0500

    finish shapes position

--- src/assets/js/shapes.js
+++ src/assets/js/shapes.js
@@ -1,21 +1,29 @@
+import peach from '../img/star-peach.svg';
+import plum from '../img/star-plum.svg';
+import craig from '../img/star-craig.png';
+import apple from '../img/star-apple.svg';
+
 $(function() {
 	if ($('.homepage')) {
-		// $('.shapes img').each(function() {
-		// 	$(this).css("top", `${$(this).data( "top" )}px`);
-		// });
-
 		const shapesContainer = $('.shapes');
 		const shapesGroup = `<div class="shapes-group">
-		<img class="parallax" src="assets/img/star-peach.svg" width="70" height="70" loading="lazy" alt="" />
-		<img class="parallax" src="assets/img/star-plum.svg" width="70" height="70" loading="lazy" alt="" />
-		<img class="parallax" src="assets/img/star-craig.png" width="270" height="270" loading="lazy" alt="" />
-		<img class="parallax" src="assets/img/star-apple.svg" width="170" height="170" loading="lazy" alt="" />
-		<img class="parallax" src="assets/img/star-peach.svg" width="70" height="70" loading="lazy" alt="" />
-		<img class="parallax" src="assets/img/star-apple.svg" width="170" height="170" loading="lazy" alt="" />
-	</div>`;
+			<img class="parallax" src="${peach}" width="70" height="70" loading="lazy" alt="" />
+			<img class="parallax" src="${plum}" width="70" height="70" loading="lazy" alt="" />
+			<img class="parallax" src="${craig}" width="270" height="270" loading="lazy" alt="" />
+			<img class="parallax" src="${apple}" width="170" height="170" loading="lazy" alt="" />
+			<img class="parallax" src="${peach}" width="70" height="70" loading="lazy" alt="" />
+			<img class="parallax" src="${apple}" width="170" height="170" loading="lazy" alt="" />
+		</div>`;
 
-		for (let i = 0; i < 13; i++) {
+		for (let i = 0; i < 10; i++) {
 			shapesContainer.append(shapesGroup);
 		}
+
+		$('.shapes .shapes-group').each(function( i ) {
+			let multiplyer = 3 * ( i + 1 );
+
+			$(this).find( $('img')).css('paddingBottom', `${multiplyer}px`);
+		});
+
 	}
 });
